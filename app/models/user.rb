2453aac129c9e5f909a896ae959bef5e5385adbe
happiguru <hguruman@gmail.com>
2022-07-20 01:19:43 +0400

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
+  before_save :downcase_email
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validates :email, presence: true, uniqueness: true, length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :username, presence: true, length: { minimum: 3, maximum: 25 },
+                       uniqueness: { case_sensitive: false }
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
